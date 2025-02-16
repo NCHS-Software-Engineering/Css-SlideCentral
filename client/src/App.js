88221e91598a0d0b60f1661346b8d6d26fb96cfb
ZaoShang12 <142527679+ZaoShang12@users.noreply.github.com>
2025-02-15 18:35:17 -0600

    Everything but tutorial is set up. Will implement these changes wit home page later

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import WidgetPage from './pages/WidgetPage';
 import FAQ from './pages/faq';
-import HomePage from './pages/test';
+
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/faq" element={<FAQ />} />
-      <Route path="/" element={<HomePage />} />
-  <Route exact path="/widgets" element={<WidgetPage />} />
+      <Route path="/faq" element={<FAQ />} />  {/* route to FAQ page */}
+      <Route path="/" element={< f/>} /> {/* needs to be intergrated with home page*/}
+  <Route exact path="/widgets" element={<WidgetPage />} /> {/* route to widget page */}
       </Routes>
 
       
