41f72ab6bcb6b91dbb6b93242c5ca46a781ffe87
Jinan Parves <jsparves@stu.naperville203.org>
2025-02-19 14:23:12 -0600

    Updated calendar Page

---
+++ client/src/pages/Calendar.js
@@ -0,0 +1,17 @@
+
+import '../styles/calendarStyles.css';
+ 
+ function Calendar() {
+    return (
+        <>
+        <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+/>
+        <link rel="stylesheet" href="styles.css"></link>
+        <h1 class="page-title">Calendar</h1>
+       
+    </>
+    );
+}
+export default Calendar;
