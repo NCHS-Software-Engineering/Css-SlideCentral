896c35c64516fdc5b0bcb9b003ff89edf4cbdc75
Isaac Liu <isliu@stu.naperville203.org>
2025-03-18 14:18:02 -0500

    google api setup

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,21 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
+const route = express.Router();
 
+// Google Auth Library for verifying tokens
+const { OAuth2Client } = require('google-auth-library');
 
+const oAuth2 = new OAuth2Client(CLIENT_ID);
+
+const jwt = require('jsonwebtoken'); // Add JWT for creating tokens
+require('dotenv').config(); // For environment variables
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
+route.get("/", (req, res) => {
+  res.render("auth");
+});
 // Middleware setup
 app.use(cors());
 app.use(express.json());
@@ -18,6 +31,3 @@ app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
 
-app.get('/Learn more about widgets', (req, res) => {
-    window.open('/widgetPage.js', '_blank');
-});
