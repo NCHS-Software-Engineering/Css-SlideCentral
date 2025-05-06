58354690a6f6abd4697a66b5c23e7477192dfbc9
Jinan Parves <jsparves@stu.naperville203.org>
2025-05-06 14:03:27 -0500

    Update server.js to have DB_PASS

--- Server/server.js
+++ Server/server.js
@@ -39,7 +39,7 @@ const db = mysql.createConnection({
   host:     process.env.DB_HOST,
   port:     parseInt(process.env.DB_PORT, 10) || 3306,
   user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 db.connect(err => {
